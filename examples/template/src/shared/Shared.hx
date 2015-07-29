e185b0dec4ef8b8f16bc9812ea2fe227c9d46af5
TiagoLr <prog4mr@gmail.com>
2015-07-29 02:08:30 +0100

    removed comments

--- examples/template/src/shared/Shared.hx
+++ examples/template/src/shared/Shared.hx
@@ -7,11 +7,5 @@ class Shared {
     static public function init() {
 		Tasks.init();
 		AppRouter.init();
-		
-		// make collections available as global vars
-		
-		if (Tasks.collection.find().count() == 0) {
-			// create stub tasks
-		}
     }
 }
